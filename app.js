79244bec5afd172f36311251ab0beed031e1e82e
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2016-10-12 10:51:12 -0500

    scattered cleanup commit here.

--- app.js
+++ app.js
@@ -18,11 +18,10 @@ var trello = new Trello(nconf.get('trello:key'),nconf.get('trello:token'));
 var listnames = [];
 var validColors = [];
 
-var defaultfn = 'result';
+var defaultfilename = 'result';
 var defaultappend = '_1';
 var awsloc = "";
 //var awsloc = 'https://s3.amazonaws.com/erf-materials/';
-//var speclistname = nconf.get('')
 
 //Example URL:  https://trello.com/b/yeaRDUaD/work-description
 //OR:           https://trello.com/b/yeaRDUaD
@@ -36,7 +35,6 @@ var awsloc = "";
     // etc...
     
     // Users can send a board id or a full url, so...
-//    Execute(event);
 //};
 
 //TEST URL
@@ -72,13 +70,14 @@ function Execute(query){
     //nconf.file({file:'https://s3.amazonaws.com/erf-materials/trelloconfig.json'});
     nconf.file('spec', awsloc + specname);
     listnames = nconf.get('spec:listnames');
+    //Some cause for concern here. We could go barreling past this before the config file comes back.
     
     //TODO: track an error string through this to intelligently help users who didnt quite nail the spec
     trello.getListsOnBoard(params.targetboard, function(error, lists){
             if(!error){
                 var All = [];
                 var validationResult = [];
-                if(typeof lists == 'string') return; //sometimes trello's api will just chuck an error string back at you.
+                if(typeof lists == 'string') return; //sometimes trello's api will just chuck an error string back at you. Maybe that's my fault?
                 async.filter(lists, function(list, topcallback){
                     var validation;
                     if(ValidateName(list.name) == true){
@@ -88,13 +87,11 @@ function Execute(query){
                         trello.getCardsOnList(list.id, function(err, data){
                             if(!err){
                                 async.filter(data, function(card, bottomcallback){
-                                    //TODO: attach metadata for lists
                                     var Card = new Object();
                                     var clr;
                                     
-                                    //maybe try if(params.isColorCoded && IsCCListName(list.name)){...}
-                                    //TODO: this is a localization loose end here...
-                                    if(params.isColorCoded && list.name.toLowerCase() == "color code"){
+                                    //In json spec,  color code list name is always first. It's either that or create another property in the spec object.
+                                    if(params.isColorCoded && list.name.toLowerCase() == listnames[0]){
                                         //store color codes in an array so we can check for valid application of the colors in the cards
                                         clr = null;
                                         if(card.labels.length > 0) clr = (card.labels[0].color) ? card.labels[0].color : 'none';
@@ -136,7 +133,12 @@ function Execute(query){
                         if(colorvalidation) validationResult += colorvalidation.toString();
                         console.log(validationResult);
                         ResolveListOrder(listnames, data, function(err, ordereddata){
-                            CreatePDF(ordereddata, defaultfn);
+                            if(validationResult.length ==0) {
+                                CreatePDF(ordereddata, defaultfilename);
+                            }
+                            else{
+                                SendValidationReport(validationResult);
+                            }
                         });
                     });
                 });
@@ -200,11 +202,6 @@ function ValidateDataAgainst(val_obj, entry){
         }
     }
     return result;
-};
-
-//Just check the name of a list against the current name of the CC list. Mainly for localization.
-function IsCCListName(name){
-
 };
 
 //Doublecheck use of colors on cards. The spec allows for color coding but only when used declaratively.
